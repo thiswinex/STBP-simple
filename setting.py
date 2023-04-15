a15de7c0632860434c2ac15d255defb4ea86a54d
thiswinex <zbhhan@gamil.com>
2023-04-15 21:42:07 +0800

    fix

--- setting.py
+++ setting.py
@@ -1,6 +1,6 @@
-DEFAULT_STEPS = 2                           # 时间步数
-DEFAULT_DT = 5                              # 时间步长，仅在时序（DVS等）数据集下有意义
-DEFAULT_SIMWIN = DEFAULT_DT * DEFAULT_STEPS # 仿真时间窗口
-DEFAULT_ALPHA = 0.5                         # 梯度近似项
-DEFAULT_VTH = 0.2                           # 阈值电压 V_threshold
-DEFAULT_TAU = 0.25                          # 漏电常数 tau
+STEPS = 2                           # 时间步数
+DT = 5                              # 时间步长，仅在时序（DVS等）数据集下有意义
+SIMWIN = DT * STEPS # 仿真时间窗口
+ALPHA = 0.5                         # 梯度近似项
+VTH = 0.2                           # 阈值电压 V_threshold
+TAU = 0.25                          # 漏电常数 tau
